5fb09afa7557c9fa0e31f45b4392048d1984e077
StayCoo1 <84853136+StayCoo1@users.noreply.github.com>
2022-10-28 16:14:11 +0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@ const init = () => {
   const myCollection = new ymaps.GeoObjectCollection({}, {
     draggable: false,
     iconLayout: 'default#image',
-    iconImageHref: "../sections/map/content/marker.svg",
+    iconImageHref: "../sections/map/content/marker1.svg",
     iconImageSize: [46, 57],
     iconImageOffset: [-35, -52]
   });
